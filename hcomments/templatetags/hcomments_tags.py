c864664d6c2f96439b4d8a840c0d07ba8487a2b0
David Mugnai <dvd@gnx.it>
2011-04-10 02:11:23 +0200

    aggiunta la possibilità di identificare l'owner dell'oggetto

--- hcomments/templatetags/hcomments_tags.py
+++ hcomments/templatetags/hcomments_tags.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 from django import template
 from django.contrib.contenttypes.models import ContentType
+from django.template import Context
 
 import mptt
 import mptt.utils
@@ -49,12 +50,16 @@ def get_comment_list(parser, token):
     return Node(object, var_name)
 
 @register.inclusion_tag('hcomments/show_comment_list.html', takes_context = True)
-def show_comment_list(context, object):
-    context.update({ 'comments': _get_comment_list(object) })
-    return context
+def show_comment_list(context, object, object_owner=None):
+    ctx = Context(context)
+    ctx.update({
+        'comments': _get_comment_list(object),
+        'object_owner': object_owner,
+    })
+    return ctx
 
 @register.inclusion_tag('hcomments/show_single_comment.html', takes_context = True)
-def show_single_comment(context, comment):
+def show_single_comment(context, comment, object_owner=None):
     request = context['request']
     if 'user-comments' in request.session:
         owner = comment.id in request.session['user-comments']
@@ -63,6 +68,7 @@ def show_single_comment(context, comment):
     return {
         'c': comment,
         'owner': owner,
+        'object_owner': object_owner,
     }
 
 @register.inclusion_tag('hcomments/show_comment_form.html', takes_context=True)
